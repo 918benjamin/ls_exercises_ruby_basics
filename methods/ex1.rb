b4d897de83ba1e4879fe99c38ed37268838ca056
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-10-02 10:33:16 -0400

    Add completed Methods exercises

---
+++ methods/ex1.rb
@@ -0,0 +1,7 @@
+# Write a method named print_me that prints "I'm printing within the method!" when invoked.
+
+def print_me() # didn't need the parentheses on this method deifnition
+  puts "I'm printing within the method!"
+end
+
+print_me
