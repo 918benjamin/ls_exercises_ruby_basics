a287300841285b9724ed26bdc581877c3d3baca0
Benjamin Perrault <benjamin.perrault@gmail.com>
2020-10-03 15:07:14 -0400

    Add completed Hashes exercises

---
+++ hashes/ex9.rb
@@ -0,0 +1,8 @@
+# Create a nested hash using the following data.
+
+multiple_cars = {
+                  car:   {type: "sedan", color: "blue", year: 2003},
+                  truck: {type: "pickup", color: "red", year: 1998}
+}
+
+puts multiple_cars
